bad72da879d8ab26c5797bce6ded324c6d676b15
hoanguyen <hoanguyen.it@imap.edu.vn>
2020-05-20 14:17:54 +0700

    Duyệt / Từ chối duyệt đơn

--- app/TicketApprove.php
+++ app/TicketApprove.php
@@ -29,8 +29,8 @@ class TicketApprove
         //         'manager_id' => $params['manager_id']
         //     ]);
         return \DB::table('ticket')
-            ->where(['ticket_id' => $params['ticket_id'],'status' => 0])
-            ->update(['status' => 200]);
+            ->where(['ticket_id' => $params['ticket_id'],'status' => 'open'])
+            ->update(['status' => 'approved']);
 
     }
     public function getFolowId($params) {
@@ -42,7 +42,7 @@ class TicketApprove
     public function reject($params) {
         //active 
         return \DB::table('ticket')
-            ->where(['ticket_id' => $params['ticket_id'],'status' => 0])
-            ->update(['status' => -1]);
+            ->where(['ticket_id' => $params['ticket_id'],'status' => 'open'])
+            ->update(['status' => 'rejected']);
     } 
 }
