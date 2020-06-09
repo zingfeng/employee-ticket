f24d08fe713af287a7d73c7f72b9d5d12825c24e
hoanguyen <hoanguyen.it@imap.edu.vn>
2020-06-09 12:31:07 +0700

    Add ticket process

--- app/Listeners/TicketCreateListener.php
+++ app/Listeners/TicketCreateListener.php
@@ -7,9 +7,12 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Ticket;
 use App\TicketType;
+use App\TicketProcess;
+use App\Employee;
 
 class TicketCreateListener
 {
+    public $queue = 'employee_ticket';
     /**
      * Create the event listener.
      *
@@ -44,6 +47,50 @@ class TicketCreateListener
             return false;
         }
         /// assign ticket to manager
-        $intLevel = $ticketTypeDetail->level_approve;
+        //Sau khi tạo ticket xong -> tạo process (nếu có)
+        $i = 1;
+        if($ticketTypeDetail->level_approve){     //Nếu tồn tại cấp quản lý duyệt
+            $employee_id = $data['employee_id'];
+            $batch = array();
+            for($i; $i <= $ticketTypeDetail->level_approve; $i++){
+                //Get manager of employee
+                $profile = (new Employee())->getDetailById($employee_id);
+                if(!$profile->manager_id){
+                    break;
+                }
+                //Tạo process
+                $batch[] = array(
+                    'manager_id' => $profile->manager_id,
+                    'ticket_id' => $ticketId,
+                    'status' => $i == 1 ? 'active' : 'inactive',
+                );
+                $employee_id = $profile->manager_id;
+            }
+            if($batch){
+                (new TicketProcess())->add_batch($batch);
+            }
+            
+        }
+        //Get ticket type to manager
+        $arrManager = (new TicketType())->getTicketTypeToManager((int)$data['type_id'])->toArray();
+        if($arrManager){     //Các quản lý là người duyệt trực tiếp
+            $batch2 = array();
+            foreach($arrManager as $manager){
+                if(!$manager->manager_id){
+                    break;
+                }
+                //Tạo process
+                $batch2[] = array(
+                    'manager_id' => $manager->manager_id,
+                    'ticket_id' => $ticketId,
+                    'status' => $i == 1 ? 'active' : 'inactive',
+                );
+            }
+            if($batch2){
+                (new TicketProcess())->add_batch($batch2);
+            }
+        }
+
+        return true;
     }
 }
